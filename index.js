cface6b6b63cacb9a74589e782733cd5a74bdb5c
fazlie <fazlie14@gmail.com>
2026-02-19 01:32:32 +0800

    done function playround comparing input between compChoice and HumanChoice

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ let compScore = 0
 function getComputerChoice(){
   
   const random = Math.floor(Math.random()*3)+1
+  let message = ""
 
   if(random === 1){
-    return "rock"
+    return message = "rock"
   }else if(random === 2){
-    return "paper"
+    return message = "paper"
 
   }else if(random === 3){
-    return "scissors"
+    return message = "scissors"
   }
 
 }
@@ -33,8 +34,36 @@ function getHumanChoice(){
 
 }
 
-getHumanChoice()
-const humanSelect = getHumanChoice()
-const computerSelect = getComputerChoice()
-console.log(computerSelect)
-console.log(humanSelect)
+function playRound(humanChoice, computerChoice){
+  let message = ""
+
+
+  if(humanChoice === computerChoice){
+    console.log(message = `Tied`)
+    return;
+  }
+    if((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")){
+      console.log(message = `${humanChoice} beats ${computerChoice}`)
+      humanScore++
+      return;
+
+    }else{ 
+          console.log(message = `${computerChoice} beats ${humanChoice}`)
+          compScore++
+          return
+
+
+    }
+
+
+  
+
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
